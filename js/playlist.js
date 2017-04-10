5d8bd3d3f27485787862728ebebc2df6a5a05365
timchoh585 <timchoh@gmail.com>
2017-04-09 22:27:10 -0500

    new sermon

--- js/playlist.js
+++ js/playlist.js
@@ -20,6 +20,12 @@ jQuery(function ($) {
             mediaPath = 'https://s3.us-east-2.amazonaws.com/sermons.cityhillchurch.org/Sermons/',
             extension = '',
             tracks = [{
+                "date": "April 9, 2017",
+                "passage": "Mark 1:35-39 'The Priority of Jesus'",
+                "speaker": "Pastor Robin Cho",
+                "length": "37:12",
+                "file": "Mark+1_35-39"
+            }, {
                 "date": "April 2, 2017",
                 "passage": "1 Peter 2:11-12 'As a People out of this World'",
                 "speaker": "Pastor David Lee (Guest Preacher)",
